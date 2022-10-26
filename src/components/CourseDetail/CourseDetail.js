96f9e66e3108ced6a004f2cd2c59b4084f14cff5
nasimbillah <nasimbillah269@gmail.com>
2022-10-26 17:50:14 +0600

    added pdf download option

--- src/components/CourseDetail/CourseDetail.js
+++ src/components/CourseDetail/CourseDetail.js
@@ -4,6 +4,12 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link, useLoaderData } from 'react-router-dom';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
+import { HiDownload } from "react-icons/hi";
+
+import Pdf from "react-to-pdf";
+
+// import "./styles.css";
+const ref = React.createRef();
 
 const CourseDetail = () => {
     const course = useLoaderData();
@@ -17,17 +23,28 @@ const CourseDetail = () => {
                 </Col>
                 <Col lg="9">
                     <Card className='bg-light '>
-                        <Card.Header>{title}</Card.Header>
+                        <Card.Header>
+                            <div className='d-flex justify-content-between'>
+                                <p className='m-0 fw-bold fs-2'>{title}</p>
+
+                                <Pdf targetRef={ref} filename="code-example.pdf">
+                                    {({ toPdf }) => <HiDownload onClick={toPdf} className='fw-bold text-danger fs-2'></HiDownload>}
+                                </Pdf>
+                            </div>
+                        </Card.Header>
                         <Card.Body>
-                            <Image className='w-100  rounded ' style={{ height: '355px' }} src={picture}></Image>
-                            <Card.Text>
+                            <div ref={ref}>
+                                <Image className='w-100  rounded ' style={{ height: '355px' }} src={picture}></Image>
+                                <Card.Text>
+                                    <div>
+                                        <p>{description}</p>
+
+                                    </div>
+                                </Card.Text>
                                 <div>
-                                    <p>{description}</p>
+                                    <Image style={{ width: "56px", height: "56px" }} roundedCircle src={instractorPicture}></Image>
+                                    <span>{name}</span>
                                 </div>
-                            </Card.Text>
-                            <div>
-                                <Image style={{ width: "56px", height: "56px" }} roundedCircle src={instractorPicture}></Image>
-                                <span>{name}</span>
                             </div>
                             <Link to={`/course/checkout/${id}`}><Button className='mt-3 w-100' variant="outline-primary">Get premium access</Button></Link>
                         </Card.Body>
